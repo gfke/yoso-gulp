f2867205d1ca38668d4173ed2fee42662a261295
Lukas Zech <lukas-zech-software@users.noreply.github.com>
2015-02-19 14:56:01 +0100

    Add comments and update Readme

--- tasks/build-release-scripts.js
+++ tasks/build-release-scripts.js
@@ -5,7 +5,11 @@ var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var ngAnnotate = require('gulp-ng-annotate');
 
-//Minify browserify bundle and templateCache
+/**
+ * Minify browserify bundle and templateCache
+ * After that add the cache key to the filename
+ * and copy it to the app folder
+ */
 module.exports = gulp.task('BuildReleaseScripts', function () {
     return gulp.src(global.config.paths.temp.scripts)
         .pipe(ngAnnotate())
