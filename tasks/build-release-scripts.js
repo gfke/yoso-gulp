e4b1eb20aea95614e21d595e216bc3e1a65ed821
Lukas Zech <Luke@Lukass-MacBook-Pro.local>
2015-03-16 16:45:51 +0100

    Move autoprefixer from release styles to develop styles

--- tasks/build-release-scripts.js
+++ tasks/build-release-scripts.js
@@ -1,19 +1,16 @@
 'use strict';
 
-var gulp       = require('gulp'),
-    rename     = require('gulp-rename'),
-    uglify     = require('gulp-uglify'),
-    ngAnnotate = require('gulp-ng-annotate');
-
+var gulp         = require('gulp'),
+    rename       = require('gulp-rename'),
+    minifyCSS    = require('gulp-minify-css');
 /**
- * Minify browserify bundle and templateCache
+ * Minify CSS bundle
  * After that add the cache key to the filename
  * and copy it to the app folder
  */
-module.exports = gulp.task('build-release-scripts', function () {
-    return gulp.src(global.config.paths.temp.scripts)
-        .pipe(ngAnnotate())
-        .pipe(uglify((global.config.uglify)))
-        .pipe(rename(global.config.filenames.release.scripts))
+module.exports = gulp.task('build-release-styles', function () {
+    return gulp.src(global.config.paths.temp.styles)
+        .pipe(minifyCSS((global.config.minifyCss)))
+        .pipe(rename(global.config.filenames.release.styles))
         .pipe(gulp.dest(global.config.folders.release));
 });
--- tasks/build-styles.js
+++ tasks/build-styles.js
@@ -1,9 +1,10 @@
 'use strict';
 
-var gulp   = require('gulp'),
-    concat = require('gulp-concat'),
-    sass   = require('gulp-sass'),
-    merge  = require('merge-stream');
+var gulp         = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    concat       = require('gulp-concat'),
+    sass         = require('gulp-sass'),
+    merge        = require('merge-stream');
 
 function handleError(err) {
     console.error(err.toString());
@@ -12,7 +13,7 @@ function handleError(err) {
 
 /**
  * Compiles the application styles and concatenates them with the styles
- * previously build from the dependencies
+ * previously build from the dependencies and adds vendor prefixes to both
  */
 module.exports = gulp.task('build-styles', ['resolve-js-and-css-dependencies'], function () {
     var appStyles = gulp.src(global.config.paths.source.styles)
@@ -21,6 +22,7 @@ module.exports = gulp.task('build-styles', ['resolve-js-and-css-dependencies'], 
     var dependencyStyles = gulp.src(global.config.paths.temp.styles);
 
     return merge(appStyles, dependencyStyles)
+        .pipe(autoprefixer(global.config.autoPrefixer))
         .pipe(concat(global.config.filenames.temp.styles))
         .pipe(gulp.dest(global.config.folders.temp));
 
