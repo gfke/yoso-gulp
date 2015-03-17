654254b22aaafd6983aaf891af3d33b853eeb08e
Lukas Zech <Luke@Lukass-MacBook-Pro.local>
2015-03-17 20:02:33 +0100

    Restore broken build-release-styles task

--- tasks/build-release-scripts.js
+++ tasks/build-release-scripts.js
@@ -1,16 +1,19 @@
 'use strict';
 
-var gulp         = require('gulp'),
-    rename       = require('gulp-rename'),
-    minifyCSS    = require('gulp-minify-css');
+var gulp       = require('gulp'),
+    rename     = require('gulp-rename'),
+    uglify     = require('gulp-uglify'),
+    ngAnnotate = require('gulp-ng-annotate');
+
 /**
- * Minify CSS bundle
+ * Minify webpack bundle and templateCache
  * After that add the cache key to the filename
  * and copy it to the app folder
  */
-module.exports = gulp.task('build-release-styles', function () {
-    return gulp.src(global.config.paths.temp.styles)
-        .pipe(minifyCSS((global.config.minifyCss)))
-        .pipe(rename(global.config.filenames.release.styles))
+module.exports = gulp.task('build-release-scripts', function () {
+    return gulp.src(global.config.paths.temp.scripts)
+        .pipe(ngAnnotate())
+        .pipe(uglify((global.config.uglify)))
+        .pipe(rename(global.config.filenames.release.scripts))
         .pipe(gulp.dest(global.config.folders.release));
 });
