db858754d00bde34eb4cb635293c077902eb3163
Lukas Zech <Luke@Lukass-MacBook-Pro.local>
2015-04-21 09:39:19 +0200

    Make unit-tests run in sequence

--- tasks/unit-tests.js
+++ tasks/unit-tests.js
@@ -1,24 +1,38 @@
 'use strict';
-var gulp    = require('gulp'),
-    babel   = require('gulp-babel'),
-    jasmine = require('gulp-jasmine'),
-    del     = require('del');
+var gulp        = require('gulp'),
+    babel       = require('gulp-babel'),
+    jasmine     = require('gulp-jasmine'),
+    runsequence = require('run-sequence');
 
 /**
  * This task runs your unit tests directly in node using jasmine
  * No kharma or phantomJs necessary
  * Compiles sources and test from ES6 to ES5 via babel before running them
  */
-module.exports = gulp.task('unit-test', function () {
-    gulp.src(global.config.paths.source.scripts)
-        .pipe(babel())
-        .pipe(gulp.dest(global.config.folders.tempTests));
+module.exports = gulp.task('unit-test', function (cb) {
 
-    gulp.src(global.config.paths.source.tests)
-        .pipe(babel())
-        .pipe(gulp.dest(global.config.folders.tempTests));
+    gulp.task('build-source-for-tests', function () {
+        return gulp.src(global.config.paths.source.scripts)
+            .pipe(babel())
+            .pipe(gulp.dest(global.config.folders.tempTests));
+    });
 
-    return gulp.src(global.config.paths.temp.tests)
-        .pipe(jasmine(global.config.jasmine));
+    gulp.task('build-tests', function () {
+        return gulp.src(global.config.paths.source.tests)
+            .pipe(babel())
+            .pipe(gulp.dest(global.config.folders.tempTests));
+    });
+
+    gulp.task('run-tests', function () {
+        return gulp.src(global.config.paths.temp.tests)
+            .pipe(jasmine(global.config.jasmine));
+    });
+
+    runsequence(
+        'build-source-for-tests',
+        'build-tests',
+        'run-tests',
+        cb
+    )
 
 });
