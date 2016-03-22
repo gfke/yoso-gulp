1a367ce8415b6905d2dec03c10e90f53892815df
Michael Raith <michael.raith@bcmsolutions.de>
2016-03-22 10:54:56 +0100

    add some additional meta information to index.html on build e.g. build time and version

--- tasks/build-index.js
+++ tasks/build-index.js
@@ -1,8 +1,10 @@
 'use strict';
 
-var gulpif     = require('gulp-if'),
-    replace    = require('gulp-replace'),
+var gulpif  = require('gulp-if'),
+    replace = require('gulp-replace'),
     htmlmin = require('gulp-htmlmin'),
+    path    = require('path'),
+    pkgJson = require(path.resolve('./package.json'));
 
 /**
  * This task copies the index.html file from the sources to the app folder
@@ -13,15 +15,20 @@ module.exports = function(gulp) {
     gulp.task('build-index', function () {
         var isRelease = global.config.buildProcess.isReleaseBuild;
 
-        var mainScriptTag = '<script ' + (global.config.buildProcess.addScriptElementsWithAsync ? 'async' : '') + ' src="' + global.config.filenames.release.scripts + '"></script>',
-            scriptKeyInjectTag = '<script type="application/javascript">window.gfke = {cacheKey:"' + global.config.buildProcess.cacheKey + '"}</script>';
+        var mainScriptTag      = '<script ' + (global.config.buildProcess.addScriptElementsWithAsync ? 'async' : '') + ' src="' + global.config.filenames.release.scripts + '"></script>',
+            scriptKeyInjectTag = '<script type="application/javascript">window.gfke = {cacheKey:"' + global.config.buildProcess.cacheKey + '"}</script>',
+            versionTag         = '<meta name="build-date" content="' + ((new Date()).toISOString()) + '">',
+            buildTag           = '<meta name="build-version" content="' + pkgJson.version + '">';
 
 
         return gulp.src(global.config.paths.source.index)
             // Insert link to bundled scripts, either with or without cache key
             //Also expose cache key as global
-            .pipe(replace('<!--scripts-->',
-                mainScriptTag + scriptKeyInjectTag))
+            .pipe(replace('<!--scripts-->', mainScriptTag + scriptKeyInjectTag))
+            // Add some meta information to the header.
+            .pipe(replace('<!--VERSION-->', versionTag))
+            .pipe(replace('<!--BUILD-->', buildTag))
+
             // Minify HTML
             .pipe(gulpif(isRelease, htmlmin(global.config.minifyHtml)))
 
